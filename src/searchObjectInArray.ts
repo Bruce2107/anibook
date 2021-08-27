572b55d8af76fb0f359334a9e94f6815a3800b68
Eduardo Henrique <eduardo.henrique@zup.com.br>
2021-08-27 19:25:44 -0300

    ajuste find

--- src/searchObjectInArray.ts
+++ src/searchObjectInArray.ts
@@ -1,6 +1,5 @@
 import { Link } from './types';
 
 export default function searchObjectInArray(url: string, array: Array<Link>) {
-  for (let i in array) if (array[i].url === url) return true;
-  return false;
+  return !!array.find((i) => i.url === url);
 }
